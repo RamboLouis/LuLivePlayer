d0bf7782422ae967a01d5e390aeae90a356f485e
RamboLu <coderambolu@gmail.com>
2016-06-04 16:56:13 +0800

    Found

---
+++ LuLivePlayer-OC/LuLivePlayer-OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LuLivePlayer-OC
+//
+//  Created by Louis on 16/6/4.
+//  Copyright © 2016年 RamboLu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
